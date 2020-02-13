ee7371c880f1a0937562f37c334e6a961406f1cb
Christian Trevino <ctrevino1994@yahoo.com>
2020-02-13 04:40:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-var kittens = [] //define your array here
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+] //define your array here
 
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return name;
+}
 // Add your functions and code here
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+  return name;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return name;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return name;
+}
+
+function appendKitten(name) {
+  [...kittens, "Broom"]
+  return [...kittens, "Broom"];
+}
+
+function prependKitten(name) {
+  ["Arnold", ...kittens]
+  return ["Arnold", ...kittens];
+}
+
+
+  kittens = kittens.slice(name, kittens.length - 1)
+  return kittens;
+}
+
+function removeFirstKitten(name) {
+  kittens = kittens.slice(1)
+  return name;
+}
